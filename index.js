53956555005e876074c44475bed57fab26478728
Preetham00x <preetham2006@outlook.com>
2025-12-07 18:56:51 +0530

    12

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const canvas= document.querySelector('canvas');
 const c=canvas.getContext('2d');
-canvas.width=1024;
-canvas.height=576;
+canvas.width=1920;
+canvas.height=720;
 c.fillRect(0,0,canvas.width,canvas.height);
-const gravity=0.2;
+const gravity=0.7;
 class Sprite{
-    constructor({position,velocity}){
+    constructor({position,velocity,color='green'}){
         this.position=position;
         this.velocity=velocity;
         this.height=150;
         this.lastKey;
-    }   
+        this.color=color;
+        this.attackBox={
+            width:100,
+            height:50,
+            position:this.position,
+    }}
     draw(){
-        c.fillStyle='green';
+        c.fillStyle=this.color;
         c.fillRect(this.position.x,this.position.y,50,this.height);
+        //attack box
+        c.fillStyle='yellow';
+        c.fillRect(this.attackBox.position.x,this.attackBox.position.y,this.attackBox.width,this.attackBox.height);
     }
     update(){
         this.draw();
@@ -37,12 +45,15 @@ const player=new Sprite({
 })
  player.draw();
 const enemy=new Sprite({
+    
     position:{
     x:400,
     y:100},
     velocity:{
     x:0,
-    y:0}
+    y:0},
+    color:'red'
+    
 })
  enemy.draw();
 
@@ -70,16 +81,16 @@ player.velocity.x=0;
 enemy.velocity.x=0;
 
 //player movement
-    if(keys.a.pressed && lastKey==='a'){
-        player.velocity.x=-1;
-        }else if(keys.d.pressed && lastKey==='d'){
-        player.velocity.x=1;
+    if(keys.a.pressed && player.lastKey==='a'){
+        player.velocity.x=-5;
+        }else if(keys.d.pressed && player.lastKey==='d'){
+        player.velocity.x=5;
     }
     //enemy movement
     if(keys.ArrowLeft.pressed && enemy.lastKey==='ArrowLeft'){
-        enemy.velocity.x=-1;
+        enemy.velocity.x=-5;
         }else if(keys.ArrowRight.pressed && enemy.lastKey==='ArrowRight'){
-        enemy.velocity.x=1;
+        enemy.velocity.x=5;
     }
     
 }
@@ -88,14 +99,14 @@ window.addEventListener('keydown',(event)=>{
     switch(event.key){
         case 'd':
               keys.d.pressed=true;
-              lastKey='d';
+              player.lastKey='d';
             break;
         case 'a':
            keys.a.pressed=true;
-           lastKey='a';
+           player.lastKey='a';
             break;
         case 'w':
-           player.velocity.y=-10;
+           player.velocity.y=-20;
            lastKey='w';
             break;
         case 'ArrowRight':
@@ -107,7 +118,7 @@ window.addEventListener('keydown',(event)=>{
            enemy.lastKey='ArrowLeft';
             break;
         case 'ArrowUp':
-           enemy.velocity.y=-10;
+           enemy.velocity.y=-20;
            
             break;
         }
